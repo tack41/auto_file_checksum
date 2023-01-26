00d04c31dc11feb0a31c18c69de791bca18af102
Takashi Tsuduki <tack41@gmail.com>
2023-01-27 06:27:28 +0900

    デバッグスイッチを追加し、指定されない場合の出力を抑制。 エクスプローラーからの起動した場合のために、終了後に何らかのキー入力まで画面表示を終了しないようにした 処理終了後に結果をまとめて出力するようにした

--- auto_file_checksum.ps1
+++ auto_file_checksum.ps1
@@ -1,3 +1,8 @@
+Param(
+    [Switch]$DebugOn,
+    [string]$targetPath
+)
+
 Set-StrictMode -Version latest
 $ErrorActionPreference = "Stop"
 
@@ -5,12 +10,17 @@ $scriptPath = $MyInvocation.MyCommand.Path
 $currentDir = Split-Path -Parent $scriptPath
 $parentDir = Split-Path -Parent $currentDir
 
-Write-Host  $args.Count
+$ErrorResult =  New-Object -TypeName "System.Text.StringBuilder"
+
+if($DebugOn){
+   Write-Host "Debug: ON" 
+}
 
 function SearchDirectory {
     param ($directoryInfo)
 
-    Write-Host "Directory: $($directoryInfo.FullName)"
+    if($DebugOn){Write-Host "Directory: $($directoryInfo.FullName)"}
+
     foreach($item in Get-ChildItem $directoryInfo){
         if($item -is [System.IO.DirectoryInfo]){
             SearchDirectory($item)
@@ -23,46 +33,58 @@ function SearchDirectory {
 function ProcessFile{
     param($fileInfo)
 
-    Write-Host "File: $($fileInfo.Name)"
+    if($DebugOn){Write-Host "File: $($fileInfo.Name)"}
     if($fileInfo.Name.EndsWith(".sha256")){
         $sourceFilePath = $fileInfo.FullName.Replace(".sha256","")
         if(-Not (Test-Path $sourceFilePath)){
-            Write-Host "There is no file corresponding to checksum file: $sourceFile"
-            exit 1
+            Write-Host "[NG] There is no file corresponding to checksum file: $sourceFile"
+            $ErrorResult.Append("[NG] There is no file corresponding to checksum file: $sourceFile`n")
         }else{
             $lastCheckSum = (-Split (Get-Content $fileInfo.FullName -Raw))[0]
-            Write-Host "Last Checksum of ${sourceFilePath}: $lastCheckSum"
+            if($DebugOn){Write-Host "Last Checksum of ${sourceFilePath}: $lastCheckSum"}
+            Write-Host "Creating current checksum of $sourceFilePath ..."
             $currentCheckSum = (certutil -hashfile $sourceFilePath sha256)[1]
-            Write-Host "Current Checksum of ${sourceFilePath}: $currentCheckSum"
+            if($DebugOn){Write-Host "Current Checksum of ${sourceFilePath}: $currentCheckSum"}
             if($lastCheckSum -eq $currentCheckSum){
                 Write-Host "Checksums between last and current are matched: $sourceFilePath as $currentCheckSum"
             }else{
-                Write-Host "[NG]Checksums between last and current are NOT matched: $sourceFilePath.`n  Current: $currentCheckSum`n  Last: $lastCheckSum"
+                Write-Host "[NG] Checksums between last and current are NOT matched: $sourceFilePath.`n  Current: $currentCheckSum`n  Last: $lastCheckSum"
+                $ErrorResult.Append("[NG] Checksums between last and current are NOT matched: $sourceFilePath.`n  Current: $currentCheckSum`n  Last: $lastCheckSum`n")
             }
         }
     }else{
         $checkSumFilePath = $fileInfo.FullName + ".sha256"
         if(-Not (Test-Path $checkSumFilePath)){
+            Write-Host "Creating current checksum of $($fileInfo.FullName) ..."
             $currentCheckSum = (certutil -hashfile $fileInfo.FullName sha256)[1]
-            Write-Host "Current Checksum of $($fileInfo.FullName): $currentCheckSum"
-            Write-Output $currentCheckSum $fileInfo.Name | Out-File -FilePath $checkSumFilePath
+            if($DebugOn){Write-Host "Current Checksum of $($fileInfo.FullName): $currentCheckSum"}
+            Write-Output "$currentCheckSum  $($fileInfo.Name)" | Out-File -FilePath $checkSumFilePath
             Write-Host "Check sum of $($fileInfo.FullName) written to ${checkSumFilePath}: $currentCheckSum"
         }
     }
 }
 
-foreach($arg in $args){
-    if(-Not (Test-Path $arg)){
-        Write-Host "Specified file or directory does not exists: $arg"
-    }else{
-        Write-Host "Exists: $arg"
+if(-Not (Test-Path $targetPath)){
+    Write-Host "[NG] Specified file or directory does not exists: $targetPath"
+    $ErrorResult.Append("[NG] Specified file or directory does not exists: $targetPath`n")
+}else{
+    if($DebugOn){Write-Host "Exists: $targetPath"}
 
-        $target = Get-Item $arg
-        if($target -is [System.IO.DirectoryInfo]){
-            SearchDirectory($target)
-        }else{
-            ProcessFile($target)
-        }
+    $target = Get-Item $targetPath
+    if($target -is [System.IO.DirectoryInfo]){
+        SearchDirectory($target)
+    }else{
+        ProcessFile($target)
     }
 }
 
+if($DebugOn){Write-Host "ErrorResult: $ErrorResult"}
+if($ErrorResult.Length -eq 0){
+    Write-Host "*** Succeeded ***"
+}else{
+    Write-Host "!!! Some error occured !!!"
+    Write-Host $ErrorResult 
+}
+
+Write-Host -NoNewLine 'Press any key to continue...';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
